b4e6c16546baafcbd2c454382a90b81d5502d5f3
Andrew Billiard <Andyhargrebil@gmail.com>
2019-04-29 17:48:52 -0500

    Steel Trap added

---
+++ OverwatchRip/Assets/Scripts/Steel_Trap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steel_Trap : MonoBehaviour
+{
+    public float trapCooldown;
+    public GameObject trap;
+    float trapTime;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        GetComponent<Player_InputManager>().OnSpecial += PlaceTrap;
+    }
+
+    void Update()
+    {
+        trapTime += Time.deltaTime;
+    }
+
+    void PlaceTrap()
+    {
+        if(trapTime >= trapCooldown)
+        {
+            Instantiate(trap, transform.position, transform.rotation);
+            trapTime = 0;
+        }
+    }
+}
