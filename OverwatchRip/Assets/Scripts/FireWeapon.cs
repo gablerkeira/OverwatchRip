2ff3f8c1eac9db28cfba66fdeb494224f623ac24
John <jroening@stumail.jccc.edu>
2019-04-29 17:31:53 -0500

    Grenada

--- OverwatchRip/Assets/Scripts/FireWeapon.cs
+++ OverwatchRip/Assets/Scripts/FireWeapon.cs
@@ -4,34 +4,19 @@ using UnityEngine;
 
 public class FireWeapon : MonoBehaviour
 {
-    public float grenadeTime = 3;
     public int forceFired = 1000;
     public GameObject projectile;
 
     private int fireInterval = 2;
     private float nextTimetoFire = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimetoFire)
-        {
-            nextTimetoFire = Time.time + 1 / fireInterval;
-            Fire();
-        }
-    }
-
     void Fire()
     {
-        GameObject go = Instantiate(projectile, this.transform.position, Quaternion.identity);
-        go.GetComponent<Rigidbody>().AddForce(Vector3.forward * forceFired);
-        Destroy(go, grenadeTime);
+        if (Time.time >= nextTimetoFire)
+        {
+            nextTimetoFire = Time.time + 1 / fireInterval;
+            GameObject go = Instantiate(projectile, this.transform.position, Quaternion.identity);
+            go.GetComponent<Rigidbody>().AddForce(Vector3.forward * forceFired);
+        }
     }
 }
