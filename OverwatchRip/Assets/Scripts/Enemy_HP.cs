8869e6c438dc68c071827d7de83f9dff656bedce
Zach Kao <zkao@stumail.jccc.edu>
2019-05-01 17:17:21 -0500

    Riptire and Enemy HP

---
+++ OverwatchRip/Assets/Scripts/Enemy_HP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_HP : MonoBehaviour
+{
+    public int health = 200;
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
